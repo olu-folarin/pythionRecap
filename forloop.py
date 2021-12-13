81244e59cbddf0c141e2fd27fab26497d52c19e2
folarin oyenuga <folami25@gmail.com>
2021-12-13 10:48:38 +0100

    for and while loops exercises

---
+++ forloop.py
@@ -0,0 +1,59 @@
+# for num in range(1,9):
+#     if num%2 == 0:
+#         print(f'{num} is an even number.')
+#     else:
+#         print(f'{num} is an odd number.')
+
+
+# RANGE
+# for num in range(25,-1,-1):
+#     print(num)
+
+
+# Exercise 1
+    # add up every odd number from 10 to 20
+    # store the total value in a variable
+# tots = 0
+
+# for num in range(10,21):
+#     if num%2 != 0:
+#         tots += num
+#     else:
+#         print('even')
+    
+# print(tots)
+
+
+# Exercise 2
+    # enter an integer then print something the number of integer entered: this was an interesting exercise
+# times = input('How many times do you have? ')
+# times = int(times)
+
+# for time in range(times):
+#         print('Show me your fingers!')
+
+
+# Exercise 3
+# loop through 1-20 (inclusive)
+    # for 4 and 13, print 'the number is unlucky'
+    # for even numbers, 'x is even'
+    # for odd numbers, 'x is odd'
+    
+# for num in range(1,21):
+#     if num == 4 or num == 13:
+#         print(f'{num} is unlucky.')
+#     elif num % 2 == 0:
+#         print(f'{num} is even.')
+#     else:
+#         print(f'{num} is odd.')
+
+            # OR
+
+for num in range(1,21):
+    if num == 4 or num == 13:
+        status = 'unlucky'
+    elif num % 2 == 0:
+        status = 'even'
+    else:
+        status = 'odd'
+    print(f'{num} is {status}')
---
+++ whileloop.py
@@ -0,0 +1,47 @@
+# fav_food = input('What\'s your favorite food? ')
+
+# while fav_food !='beans':
+#     print('Incorrect!')
+#     fav_food = input('What\'s your favorite food? ')
+# print('On point!')
+
+# num = 1
+
+# while num < 11:
+#     print(num)
+#     num += 1
+
+
+# Exercise 1
+# print the emoji ('\U0001f600') using a for loop and a while loop from 1 to 10 in a leftt-angle triangular shape.
+
+        # with a while loop
+# counts = 1
+# emoji = ''
+# while counts < 11:
+#     print(emoji)
+#     counts += 1
+#     emoji += '*'
+        # OR
+# counts = 1
+
+# while counts < 12:
+#     print('*' * counts)
+#     counts += 1
+
+        # with a for loop
+# pyramid = ''
+
+# for num in range(1,11):
+#     if num < 11:
+#         pyramid += '*'
+#         print(pyramid)
+#     else:
+#         print('the end!')
+# print(pyramid)
+
+        # OR
+# counts = ''
+# for n in range(1,12):
+#     counts += '*'
+#     print(counts)
