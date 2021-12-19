e4b210ca697f30e818488388cb55397bb65a391f
folarin oyenuga <folami25@gmail.com>
2021-12-19 21:26:39 +0100

    learned how to use conditional logic in LC

--- compre.py
+++ compre.py
@@ -5,5 +5,22 @@ cars = ['volvo', 'peugeot', 'aston martin', 'range rover']
 nums = [1,4,6,7,3,8]
 # below is what it looks like
 # new_cars = [car[0].upper() + car[1:] for car in cars]
-added_num = [num + num for num in nums]
-print(added_num)
+# added_num = [num + num for num in nums]
+# print(added_num)
+
+
+
+
+# list comprehension with conditional logic
+# add_num = [num + num for num in nums]
+# print(add_num)
+
+# list comp with an if statement
+# even = [num for num in nums if num % 2 == 0]
+# start_with_v = [car for car in cars if car[0] != 'v']
+
+
+# list comp with double logical statements
+# odd = [num+num if num % 2 != 0 else 0 for num in nums]
+even = [num * 10 if num % 2 == 0 else 'odd' for num in nums]
+print(even)
