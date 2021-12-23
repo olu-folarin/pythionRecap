aa6d5345ee71d558c4485370dae431217de6dc39
folarin oyenuga <folami25@gmail.com>
2021-12-23 11:21:58 +0100

    began relearning the basics of dictionaries

---
+++ dict1.py
@@ -0,0 +1,9 @@
+friends = [
+    {'name': 'Kola', 'age': 32, 'gender': 'male', 'occupation': 'Accountant', 'location': 'UK'},
+    {'name': 'Ik', 'age': 32, 'gender': 'male', 'occupation': 'DevOps Engineer', 'location': 'USA'}, 
+    {'name': 'Tope', 'age': 31, 'gender': 'male', 'occupation': 'Banker', 'location': 'NGR'},
+    {'name': 'Damola', 'age': 30, 'gender': 'female', 'occupation': 'Banker', 'location': 'NGR'},
+    {'name': 'Sesede', 'age': 25, 'gender': 'female', 'occupation': 'Fashion Designer', 'location': 'NGR'}
+    ]
+
+print(friends[3]['gender'])
