c48b98056489aa989180d4eea69ba8aa89f32661
folarin oyenuga <folami25@gmail.com>
2022-01-02 06:47:28 +0100

    more exercise solutions

--- funcexercises.py
+++ funcexercises.py
@@ -1,3 +1,11 @@
 # exercise 1
-def product(x,y):
-    return x*y
+# def product(x,y):
+#     return x*y
+
+
+# exercise 2: day of the week
+def return_day(num_day):
+    days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+    if num_day > 0 and num_day < len(days):
+        return days[num_day-1]
+print(return_day(3))
