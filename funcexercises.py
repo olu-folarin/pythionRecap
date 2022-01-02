9f376154a1a9c3e871724c9259007268ef5c645b
folarin oyenuga <folami25@gmail.com>
2022-01-02 07:22:44 +0100

    number comparison exercise

--- funcexercises.py
+++ funcexercises.py
@@ -12,10 +12,21 @@
 
 
 # exercise 3: last element
-items = [6,7,8,9]
-def last_element(list_item):
-    if list_item:
-        return list_item[-1]
+# items = [6,7,8,9]
+# def last_element(list_item):
+#     if list_item:
+#         return list_item[-1]
+#     else:
+#         return 'Out of range'
+# print(last_element(items))
+
+
+# number compare
+def number_compare(x,y):
+    if x > y:
+        return 'First is greater'
+    elif x < y:
+        return 'Second is greater'
     else:
-        return 'Out of range'
-print(last_element(items))
+        return 'Numbers are equal'
+print(number_compare(4.01,4))
