9794a28835d5a03e5e2e8f953e91e0c08cd135a9
folarin oyenuga <folami25@gmail.com>
2022-01-02 07:10:57 +0100

    last item in a list

--- funcexercises.py
+++ funcexercises.py
@@ -4,8 +4,18 @@
 
 
 # exercise 2: day of the week
-def return_day(num_day):
-    days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
-    if num_day > 0 and num_day < len(days):
-        return days[num_day-1]
-print(return_day(3))
+# def return_day(num_day):
+#     days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+#     if num_day > 0 and num_day < len(days):
+#         return days[num_day-1]
+# print(return_day(3))
+
+
+# exercise 3: last element
+items = [6,7,8,9]
+def last_element(list_item):
+    if list_item:
+        return list_item[-1]
+    else:
+        return 'Out of range'
+print(last_element(items))
